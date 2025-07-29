e6666c7dbdfcbc4a2caf5ad56afd4506ed7782f3
dhiman10 <dhimandas10.sec@gmail.com>
2025-07-29 11:36:01 +0200

    Add files via upload
    Set Up Project

---
+++ FireBaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseProject
+//
+//  Created by Dhiman Das on 29.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
